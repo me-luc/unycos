a2031af79082e4412bbb11260e1a1c10c0d4ccfd
Lucas Almeida <lukasalmeida372@gmail.com>
2022-12-27 23:15:56 -0300

    add comment section

---
+++ src/components/comments/Comment.js
@@ -0,0 +1,155 @@
+import styled from "styled-components";
+import { BiLike } from "react-icons/bi";
+import { HiOutlineCheckCircle as CheckCircle } from "react-icons/hi";
+
+export default function Comment({ comment }) {
+	const { author, time, commentText, profileImg, reply } = comment;
+
+	return (
+		<Page>
+			<ImageBox>
+				{profileImg ? <img src={profileImg} /> : getInitial(author)}
+			</ImageBox>
+
+			<div className="content">
+				<AuthorBox>
+					{author}
+					<span>
+						<BiLike className="react-icons" /> hace {time}
+					</span>
+				</AuthorBox>
+				<CommentText>{commentText}</CommentText>
+
+				{reply && (
+					<>
+						<Line />
+						<ReplyBox>{reply.comment}</ReplyBox>
+						<ReplyAuthorBox>
+							<CheckCircle className="react-icons" />
+							{reply.author}
+							<span>
+								{reply.job && `• ${reply.job}`}
+								<span className="time"> - hace 3 meses</span>
+							</span>
+						</ReplyAuthorBox>
+					</>
+				)}
+			</div>
+		</Page>
+	);
+
+	function getInitial(name) {
+		let nameArr = name.split(" ");
+
+		return nameArr.length === 1 ? name[0] : nameArr[0][0] + nameArr[1][0];
+	}
+}
+
+const Page = styled.li`
+	display: flex;
+	margin: 25px 0;
+
+	.content {
+		margin-left: 20px;
+		width: 670px;
+	}
+
+	.react-icons {
+		margin-right: 5px;
+	}
+`;
+
+const ImageBox = styled.figure`
+	width: 80px;
+	height: 80px;
+	border-radius: 50%;
+	background: #858479;
+
+	font-family: "Lato";
+	font-style: normal;
+	font-weight: 400;
+	font-size: 28px;
+	line-height: 32px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	letter-spacing: 2px;
+	text-transform: uppercase;
+	color: #000000;
+`;
+
+const CommentText = styled.p`
+	font-family: "Lato";
+	font-style: normal;
+	font-weight: 400;
+	font-size: 16px;
+	line-height: 20px;
+	color: #d8d7ce;
+`;
+
+const AuthorBox = styled.h3`
+	font-family: "Lato";
+	font-style: normal;
+	font-weight: 400;
+	font-size: 18px;
+	line-height: 24px;
+	letter-spacing: 2px;
+	text-transform: uppercase;
+	color: #d8d7ce;
+	display: flex;
+	align-items: center;
+	margin-bottom: 15px;
+
+	span {
+		line-height: 28px;
+		color: #858479;
+		display: flex;
+		align-items: center;
+		margin-left: 15px;
+	}
+`;
+const ReplyBox = styled.p`
+	font-family: "Lato";
+	font-style: normal;
+	font-weight: 400;
+	font-size: 16px;
+	line-height: 20px;
+	color: #858479;
+`;
+const ReplyAuthorBox = styled.h3`
+	font-family: "Lato";
+	font-style: normal;
+	font-weight: 700;
+	font-size: 16px;
+	line-height: 20px;
+	color: #f2ecc9;
+	display: flex;
+	align-items: center;
+	margin: 5px 0;
+
+	span {
+		font-family: "Lato";
+		font-style: normal;
+		font-weight: 400;
+		font-size: 16px;
+		line-height: 20px;
+		color: #c5af19;
+		margin-left: 5px;
+		display: flex;
+		align-items: center;
+	}
+	.time {
+		color: #858479;
+	}
+	.react-icons {
+		color: #c5af19;
+		font-size: 20px;
+	}
+`;
+
+const Line = styled.div`
+	width: 100%;
+	height: 1px;
+	background: #858479;
+	margin: 15px 0;
+`;
--- src/components/read-more/ReadMore.js
+++ src/components/read-more/ReadMore.js
@@ -1,3 +1,72 @@
+import styled from "styled-components";
+import imgPoster from "../../assets/read-more-img.png";
+
 export default function ReadMore() {
-	return <h1>READ MORE</h1>;
+	return (
+		<Poster>
+			<Title>APRENDE CON LOS MEJORES</Title>
+			<Message>
+				Detrás de cada éxito, hay una história. conoce en nuestro blog.
+			</Message>
+			<StyledButton>leer más en nuestro blog</StyledButton>
+		</Poster>
+	);
 }
+
+const Poster = styled.section`
+	width: 100vw;
+	height: 345px;
+	background-image: url(${imgPoster});
+	background-repeat: no-repeat;
+	background-size: cover;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: space-evenly;
+	align-items: center;
+	margin-bottom: 45px;
+`;
+
+const Title = styled.h2`
+	font-family: "Lato";
+	font-style: normal;
+	font-weight: 400;
+	font-size: 32px;
+	line-height: 38px;
+	text-transform: uppercase;
+	color: #ffffff;
+`;
+
+const Message = styled.p`
+	font-family: "Lato";
+	font-style: normal;
+	font-weight: 400;
+	font-size: 24px;
+	line-height: 29px;
+	text-transform: uppercase;
+	color: #ffffff;
+`;
+
+const StyledButton = styled.button`
+	background: none;
+	border: 2px solid #f1f1f1;
+	border-radius: 4px;
+	width: 370px;
+	height: 50px;
+
+	font-family: "Lato";
+	font-style: normal;
+	font-weight: 700;
+	font-size: 16px;
+	line-height: 20px;
+	display: flex;
+	align-items: center;
+	letter-spacing: 1px;
+	text-transform: uppercase;
+	color: #f1f1f1;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	cursor: pointer;
+`;
